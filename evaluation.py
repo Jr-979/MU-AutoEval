a1ef1361e14fd81d32a34b435fd592a21691c56b
Jr-979 <nuchpol@gmail.com>
2022-09-02 23:14:03 +0700

    Reduce a number of line with for loops

--- evaluation.py
+++ evaluation.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common import exceptions as Exceptions
 import chromedriver_autoinstaller
 import time
 import random
@@ -31,89 +32,85 @@ def main():
     driver.get("http://www.student.mahidol.ac.th/evaluation/index.asp?cookie-lost")
     driver.set_window_size(1280, 715)
 
-    try:
-        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CSS_SELECTOR, "td:nth-child(1) > a > b")))
-        print ("Page is ready!")
-        driver.find_element(By.CSS_SELECTOR, "td:nth-child(1) > a > b").click()
-        current_Sem = driver.find_element(By.CSS_SELECTOR, "td:nth-child(1) > a > b").text
+    # Wait for 60 second for enter username and password
+    WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.CSS_SELECTOR, "td:nth-child(1) > a > b")))
+    print ("Page is ready!")
+    driver.find_element(By.CSS_SELECTOR, "td:nth-child(1) > a > b").click()
 
-        time.sleep(1)
-        user_info = Convert(driver.find_element_by_xpath("/html/body").text)
+    # Get the most recent semester 
+    current_Sem = driver.find_element(By.CSS_SELECTOR, "td:nth-child(1) > a > b").text
 
+    time.sleep(1)
+    user_info = Convert(driver.find_element_by_xpath("/html/body").text)
 
-        subject = []
-        for text in user_info:
-            if text.isupper():
-                subject.append(text)
+    subject = []
+    for text in user_info:
+        if text.isupper():
+            subject.append(text)
 
-        subject_code = subject[1::2]
+    # Store all the avaliable subject as a subject code
+    subject_code = subject[1::2]
 
+    # Loops through all the subject code 
+    for subject in subject_code:
+        driver.get(f"http://www.student.mahidol.ac.th/evaluation/evaluate.asp?cid={subject}&fid=EG006C&tid=EG005T&mid=0&quarter={current_Sem[-2::1]}{current_Sem[0]}")
+        driver.set_window_size(1280, 715)
 
-        for subject in subject_code:
-            driver.get(f"http://www.student.mahidol.ac.th/evaluation/evaluate.asp?cid={subject}&fid=EG006C&tid=EG005T&mid=0&quarter={current_Sem[-2::1]}{current_Sem[0]}")
-            driver.set_window_size(1280, 715)
-            if not "นักศึกษาประเมินรายวิชาเสร็จเรียบร้อยแล้ว" in driver.page_source:
-                driver.find_element(By.CSS_SELECTOR, f"tr:nth-child(3) .scorePic:nth-child({ranBias()})").click()
-                driver.find_element(By.CSS_SELECTOR, f"tr:nth-child(4) .scorePic:nth-child({ranBias()})").click()
-                driver.find_element(By.CSS_SELECTOR, f"tr:nth-child(5) .scorePic:nth-child({ranBias()})").click()
-                driver.find_element(By.CSS_SELECTOR, f"tr:nth-child(6) .scorePic:nth-child({ranBias()})").click()
-                driver.find_element(By.CSS_SELECTOR, f"tr:nth-child(7) .scorePic:nth-child({ranBias()})").click()
-                driver.find_element(By.CSS_SELECTOR, f"tr:nth-child(9) .scorePic:nth-child({ranBias()})").click()
-                driver.find_element(By.CSS_SELECTOR, f"tr:nth-child(10) > #q2").click()
-                driver.find_element(By.CSS_SELECTOR, f"tr:nth-child(10) .scorePic:nth-child({ranBias()})").click()
-                driver.find_element(By.CSS_SELECTOR, f"tr:nth-child(11) .scorePic:nth-child({ranBias()})").click()
-                driver.find_element(By.CSS_SELECTOR, f"tr:nth-child(12) .scorePic:nth-child({ranBias()})").click()
-                driver.find_element(By.CSS_SELECTOR, f"tr:nth-child(13) .scorePic:nth-child({ranBias()})").click()
-                driver.find_element(By.CSS_SELECTOR, f"tr:nth-child(14) .scorePic:nth-child({ranBias()})").click()
-                driver.find_element(By.CSS_SELECTOR, f"tr:nth-child(15) .scorePic:nth-child({ranBias()})").click()
-                driver.find_element(By.CSS_SELECTOR, f"tr:nth-child(16) .scorePic:nth-child({ranBias()})").click()
-                driver.find_element(By.CSS_SELECTOR, f"tr:nth-child(18) .scorePic:nth-child({ranBias()})").click()
-                driver.find_element(By.CSS_SELECTOR, f"tr:nth-child(19) > #q3").click()
-                driver.find_element(By.CSS_SELECTOR, f"tr:nth-child(19) .scorePic:nth-child({ranBias()})").click()
-                driver.find_element(By.CSS_SELECTOR, f"tr:nth-child(20) .scorePic:nth-child({ranBias()})").click()
-                driver.find_element(By.CSS_SELECTOR, f"tr:nth-child(22) .scorePic:nth-child({ranBias()})").click()
-                driver.find_element(By.CSS_SELECTOR, f"tr:nth-child(23) .scorePic:nth-child({ranBias()})").click()
-                driver.find_element(By.CSS_SELECTOR, f"td:nth-child(2) > .btnBar").click()
-                driver.switch_to.alert.text == "กรุณาตรวจสอบข้อมูลให้ถูกต้อง\nหากบันทึกผลการประเมินแล้ว ระบบจะไม่อนุญาตให้ทำการแก้ไข\n\nต้องการบันทึกผลการประเมินหรือไม่"
-                driver.switch_to.alert.accept()
+        # Check weather evaluation is complete
+        if not "นักศึกษาประเมินรายวิชาเสร็จเรียบร้อยแล้ว" in driver.page_source:
+            # Max number of checkboxes start from 3 to 23 with 10,19,2 
+            for checkbox in range(3,24,1):
+                if checkbox == 10:
+                    driver.find_element(By.CSS_SELECTOR, f"tr:nth-child(10) > #q2").click()
+                elif checkbox == 19:
+                    driver.find_element(By.CSS_SELECTOR, f"tr:nth-child(19) > #q3").click()
 
-            driver.get(f"http://www.student.mahidol.ac.th/evaluation/teacher.asp?cid={subject}&tid=EG005T&mid=0&quarter={current_Sem[-2::1]}{current_Sem[0]}")
-            while  "Click เพื่อประเมิน" in driver.page_source:
-                driver.find_element(By.LINK_TEXT, "Click เพื่อประเมิน").click()
-                element = driver.find_element(By.LINK_TEXT, "Click เพื่อประเมิน")
-                actions = ActionChains(driver)
-                actions.move_to_element(element).perform()
-                element = driver.find_element(By.CSS_SELECTOR, "body")
-                actions = ActionChains(driver)
-                time.sleep(0.5)
-                
-                for selected in range (25):
+                driver.find_element(By.CSS_SELECTOR, f"tr:nth-child({checkbox}) .scorePic:nth-child({ranBias()})").click()
+            # Press a submit button
+            driver.find_element(By.CSS_SELECTOR, f"td:nth-child(2) > .btnBar").click()
+
+            # Press an accept button
+            driver.switch_to.alert.text == "กรุณาตรวจสอบข้อมูลให้ถูกต้อง\nหากบันทึกผลการประเมินแล้ว ระบบจะไม่อนุญาตให้ทำการแก้ไข\n\nต้องการบันทึกผลการประเมินหรือไม่"
+            driver.switch_to.alert.accept()
+
+        # Loops through all available teacher
+        driver.get(f"http://www.student.mahidol.ac.th/evaluation/teacher.asp?cid={subject}&tid=EG005T&mid=0&quarter={current_Sem[-2::1]}{current_Sem[0]}")
+        while  "Click เพื่อประเมิน" in driver.page_source:
+            driver.find_element(By.LINK_TEXT, "Click เพื่อประเมิน").click()
+            element = driver.find_element(By.LINK_TEXT, "Click เพื่อประเมิน")
+            actions = ActionChains(driver)
+            actions.move_to_element(element).perform()
+            element = driver.find_element(By.CSS_SELECTOR, "body")
+            actions = ActionChains(driver)
+            time.sleep(0.5)
+            
+            # Fill each checkbox with random value
+            for selected in range (25):
+                try:
+                    driver.find_element(By.CSS_SELECTOR, f"tr:nth-child({selected}) .scorePic:nth-child({ranBias()})").click()
+                except Exceptions.NoSuchElementException:
+                    # If the element is not befound continue
+                    continue
+            
+            # Fill missing checkbox
+            for i in range (3):
+                for missing in range (25):
                     try:
-                        driver.find_element(By.CSS_SELECTOR, f"tr:nth-child({selected}) .scorePic:nth-child({ranBias()})").click()
-                    except:
+                        driver.find_element(By.CSS_SELECTOR, f"tr:nth-child({missing}) > #q{i} > .scorePic:nth-child({ranBias()})").click()
+                    except Exceptions.NoSuchElementException:
+                        # If the element is not befound continue
                         continue
-
-                for i in range (3):
-                    for missing in range (25):
-                        try:
-                            driver.find_element(By.CSS_SELECTOR, f"tr:nth-child({missing}) > #q{i} > .scorePic:nth-child({ranBias()})").click()
-                        except:
-                            continue
-                driver.find_element(By.CSS_SELECTOR, f"td:nth-child(2) > .btnBar").click()
-                assert driver.switch_to.alert.text == "กรุณาตรวจสอบข้อมูลให้ถูกต้อง\nหากบันทึกผลการประเมินแล้ว ระบบจะไม่อนุญาตให้ทำการแก้ไข\n\nต้องการบันทึกผลการประเมินหรือไม่"
-                driver.switch_to.alert.accept()
-                time.sleep(0.5)
-        
-        return True
-    except:
-        return False
-
-    driver.quit()
+            driver.find_element(By.CSS_SELECTOR, f"td:nth-child(2) > .btnBar").click()
+            assert driver.switch_to.alert.text == "กรุณาตรวจสอบข้อมูลให้ถูกต้อง\nหากบันทึกผลการประเมินแล้ว ระบบจะไม่อนุญาตให้ทำการแก้ไข\n\nต้องการบันทึกผลการประเมินหรือไม่"
+            driver.switch_to.alert.accept()
+            time.sleep(0.5)
+    
+    return 0 
 
 if __name__ == "__main__":
     print("Program Running")
-    Suscess = main()
-    if Suscess:
+    fail = main()
+    if not fail:
         print("\nSuscessfully Evaluated!!!! Thankyou for using. ")
         input("Press enter to exit the program")
     
